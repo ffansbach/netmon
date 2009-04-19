bf61cd0d3ccae7bc7193d36c4af944c0d4de1e1d
Floh1111 <Floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2009-04-19 12:49:05 +0000

    Nodelist (hoffentlich) verbessert.
    git-svn-id: http://svn.freifunk-ol.de/netmon/Trunk@9 e4279fa6-117a-4c11-8b0b-ff71f4ed7350

--- templates/html/nodelist.tpl.php
+++ templates/html/nodelist.tpl.php
@@ -2,11 +2,10 @@
 
 <h2>Nodes (Wlan)</h2>
 
-<div id="nodeitem" style="width: 895px; overflow: hidden;">
+<div id="nodeitem" style="width: 800px; overflow: hidden;">
   <div nstyle="white-space: nowrap;">
     <div style="float:left; width: 20px;"><b>#</b></div>
-    <div style="float:left; width: 95px;"><b>Node</b></div>
-    <div style="float:left; width: 95px;"><b>Link</b></div>    
+    <div style="float:left; width: 95px;"><b>Node-IP</b></div>
     <div style="float:left; width: 250px;"><b>Subnet</b></div>
     <div style="float:left; width: 110px;"><b>Inhaber</b></div>
     <div style="float:left; width: 80px;"><b>Status</b></div>
@@ -16,11 +15,10 @@
 </div>
 
 {foreach key=count item=nodelist from=$nodelist}
-<div id="nodeitem" style="width: 895px; overflow: hidden;">
+<div id="nodeitem" style="width: 800px; overflow: hidden;">
   <div style="white-space: nowrap;">
     <div style="float:left; width: 20px;"><b>{$count+1}</b></div>
-    <div style="float:left; width: 95px;"><a href="./index.php?get=node&id={$nodelist.node_id}">{$net_prefix}.{$nodelist.subnet_ip}.{$nodelist.node_ip}</a></div>
-    <div style="float:left; width: 95px;"><a href="./index.php?get=service&service_id={$nodelist.service_id}">Erw. Info</a></div>
+    <div style="float:left; width: 95px;"><a href="./index.php?get=service&service_id={$nodelist.service_id}">{$net_prefix}.{$nodelist.subnet_ip}.{$nodelist.node_ip}</a></div>
     <div style="float:left; width: 250px;"><a href="./index.php?get=subnet&id={$nodelist.subnet_id}">{$nodelist.title}</a></div>
     <div style="float:left; width: 110px;"><a href="./index.php?get=user&id={$nodelist.user_id}">{$nodelist.nickname}</a></div>
     {if $nodelist.status=="online"}
@@ -38,11 +36,10 @@
 
 <h2>VPN (Verbindungs-Netze)</h2>
 
-<div id="nodeitem" style="width: 895px; overflow: hidden;">
+<div id="nodeitem" style="width: 800px; overflow: hidden;">
   <div nstyle="white-space: nowrap;">
     <div style="float:left; width: 20px;"><b>#</b></div>
-    <div style="float:left; width: 95px;"><b>Node</b></div>
-    <div style="float:left; width: 95px;"><b>Link</b></div>    
+    <div style="float:left; width: 95px;"><b>VPN-IP</b></div>
     <div style="float:left; width: 250px;"><b>Subnet</b></div>
     <div style="float:left; width: 110px;"><b>Inhaber</b></div>
     <div style="float:left; width: 80px;"><b>Status</b></div>
@@ -52,11 +49,10 @@
 </div>
 
 {foreach key=count item=nodelist from=$vpnlist}
-<div id="nodeitem" style="width: 895px; overflow: hidden;">
+<div id="nodeitem" style="width: 800px; overflow: hidden;">
   <div style="white-space: nowrap;">
     <div style="float:left; width: 20px;"><b>{$count+1}</b></div>
-    <div style="float:left; width: 95px;"><a href="./index.php?get=node&id={$nodelist.node_id}">{$net_prefix}.{$nodelist.subnet_ip}.{$nodelist.node_ip}</a></div>
-    <div style="float:left; width: 95px;"><a href="./index.php?get=service&service_id={$nodelist.service_id}">Erw. Info</a></div>
+    <div style="float:left; width: 95px;"><a href="./index.php?get=service&service_id={$nodelist.service_id}">{$net_prefix}.{$nodelist.subnet_ip}.{$nodelist.node_ip}</a></div>
     <div style="float:left; width: 250px;"><a href="./index.php?get=subnet&id={$nodelist.subnet_id}">{$nodelist.title}</a></div>
     <div style="float:left; width: 110px;"><a href="./index.php?get=user&id={$nodelist.user_id}">{$nodelist.nickname}</a></div>
     {if $nodelist.status=="online"}
@@ -77,9 +73,8 @@
 <div id="nodeitem" style="width: 895px; overflow: hidden;">
   <div nstyle="white-space: nowrap;">
     <div style="float:left; width: 20px;"><b>#</b></div>
-    <div style="float:left; width: 95px;"><b>Node</b></div>
-    <div style="float:left; width: 95px;"><b>Link</b></div>    
-    <div style="float:left; width: 250px;"><b>Titel</b></div>
+    <div style="float:left; width: 270px;"><b>Service</b></div>
+    <div style="float:left; width: 140px;"><b>Crawl-URL</b></div>
     <div style="float:left; width: 110px;"><b>Inhaber</b></div>
     <div style="float:left; width: 80px;"><b>Status</b></div>
     <div style="float:left; width: 80px;"><b>Uptime</b></div>
@@ -91,9 +86,8 @@
 <div id="nodeitem" style="width: 895px; overflow: hidden;">
   <div style="white-space: nowrap;">
     <div style="float:left; width: 20px;"><b>{$count+1}</b></div>
-    <div style="float:left; width: 95px;"><a href="./index.php?get=node&id={$nodelist.node_id}">{$net_prefix}.{$nodelist.subnet_ip}.{$nodelist.node_ip}</a></div>
-    <div style="float:left; width: 95px;"><a href="./index.php?get=service&service_id={$nodelist.service_id}">Erw. Info</a></div>
-    <div style="float:left; width: 250px;"><a href="./index.php?get=service&service_id={$nodelist.service_id}">{$nodelist.services_title}</a></div>
+    <div style="float:left; width: 270px;"><a href="./index.php?get=service&service_id={$nodelist.service_id}">{$nodelist.services_title}</a></div>
+    <div style="float:left; width: 140px;"><a href="./index.php?get=service&service_id={$nodelist.service_id}">{$net_prefix}.{$nodelist.node_ip}.{$nodelist.subnet_ip} : {$nodelist.crawler}</a></div>
     <div style="float:left; width: 110px;"><a href="./index.php?get=user&id={$nodelist.user_id}">{$nodelist.nickname}</a></div>
     {if $nodelist.status=="online"}
       <div style="float:left; width: 80px; background-color: green;">{$nodelist.status}</div>
