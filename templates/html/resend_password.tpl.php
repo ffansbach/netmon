3c619d15abb2fcd901898a4d2ee5026e48e06fcf
Floh1111 <Floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2009-05-03 10:20:50 +0000

    Passwort zusenden und Aktivierungsmail zusenden eingebaut (vergessen Templates zu adden)
    git-svn-id: http://svn.freifunk-ol.de/netmon/Trunk@18 e4279fa6-117a-4c11-8b0b-ff71f4ed7350

---
+++ templates/html/resend_password.tpl.php
@@ -0,0 +1,4 @@
+<form action="./index.php?get=register&section=resend_password" method="POST">
+  <p>Deine Emailadresse:<br><input name="email" type="text" size="30" maxlength="50"></p>
+  <p><input type="submit" value="Passwort zusenden"></p>
+</form>
