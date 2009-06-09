b83240b910be93ecbfe3bf616315e6ac62f390ef
Floh1111 <Floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2009-06-09 11:43:56 +0000

    Get free IP-Range Bug gefixed
    git-svn-id: http://svn.freifunk-ol.de/netmon/Trunk@30 e4279fa6-117a-4c11-8b0b-ff71f4ed7350

--- templates/html/nodelist.tpl.php
+++ templates/html/nodelist.tpl.php
@@ -119,3 +119,43 @@
 {else}
 <p>Keine Nodes vorhanden</p>
 {/if}
+
+<h2>Clients</h2>
+
+<div id="nodeitem" style="width: 800px; overflow: hidden;">
+  <div nstyle="white-space: nowrap;">
+    <div style="float:left; width: 20px;"><b>#</b></div>
+    <div style="float:left; width: 95px;"><b>Node-IP</b></div>
+    <div style="float:left; width: 250px;"><b>Subnet</b></div>
+    <div style="float:left; width: 110px;"><b>Inhaber</b></div>
+    <div style="float:left; width: 80px;"><b>Status</b></div>
+    <div style="float:left; width: 80px;"><b>Uptime</b></div>
+    <div style="float:left; width: 100px;"><b>Stand</b></div>
+  </div>
+</div>
+
+{if !empty($clientlist)}
+{foreach key=count item=nodelist from=$clientlist}
+<div id="nodeitem" style="width: 800px; overflow: hidden;">
+  <div style="white-space: nowrap;">
+    <div style="float:left; width: 20px;"><b>{$count+1}</b></div>
+    <div style="float:left; width: 95px;"><a href="./index.php?get=service&service_id={$nodelist.service_id}">{$net_prefix}.{$nodelist.subnet_ip}.{$nodelist.node_ip}</a></div>
+    <div style="float:left; width: 250px;"><a href="./index.php?get=subnet&id={$nodelist.subnet_id}">{$nodelist.title}</a></div>
+    <div style="float:left; width: 110px;"><a href="./index.php?get=user&id={$nodelist.user_id}">{$nodelist.nickname}</a></div>
+    {if $nodelist.status=="online"}
+      <div style="float:left; width: 80px; background-color: green;">{$nodelist.status}</div>
+    {elseif $nodelist.status=="offline"}
+      <div style="float:left; width: 80px; background-color: red;">{$nodelist.status}</div>
+    {elseif $nodelist.status=="ping"}
+      <div style="float:left; width: 80px; background-color: #00c5cc;">{$nodelist.status}</div>
+    {elseif $nodelist.status=="unbekannt"}
+      <div style="float:left; width: 80px; background-color: #fff3c3;">{$nodelist.status}</div>
+    {/if}
+    <div style="float:left; width: 80px;">{$nodelist.uptime}</div>
+    <div style="float:left; width: 100px;">{$nodelist.crawl_time} Uhr</div>
+  </div>
+</div>
+{/foreach}
+{else}
+<p>Keine Nodes vorhanden</p>
+{/if}
