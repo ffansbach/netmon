06dd196f6d1eeae8bc08cd13848c68a532abdd0a
Floh1111 <Floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2009-09-13 20:43:04 +0000

    Layoutanpassungen

--- templates/html/desktop.tpl.php
+++ templates/html/desktop.tpl.php
@@ -25,37 +25,38 @@
 
 <h2>Meine Services</h2>
 
-<div id="nodeitem" style="width: 800px; overflow: hidden;">
+<div id="nodeitem" style="width: 745px; overflow: hidden;">
   <div nstyle="white-space: nowrap;">
-    <div style="float:left; width: 20px;"><b>#</b></div>
-    <div style="float:left; width: 95px;"><b>Node-IP</b></div>
-    <div style="float:left; width: 250px;"><b>Subnet</b></div>
-    <div style="float:left; width: 110px;"><b>Inhaber</b></div>
-    <div style="float:left; width: 80px;"><b>Status</b></div>
-    <div style="float:left; width: 80px;"><b>Uptime</b></div>
-    <div style="float:left; width: 100px;"><b>Stand</b></div>
+    <div style="float:left; width: 85px;"><b>Service</b></div>
+    <div style="float:left; width: 50px;"><b>Crawl</b></div>
+    <div style="float:left; width: 100px;"><b>user</b></div>
+    <div style="float:left; width: 60px;"><b>Status</b></div>
+    <div style="float:left; width: 150px;"><b>Stand</b></div>
+    <div style="float:left; width: 150px;"><b>Subnet</b></div>
+    <div style="float:left; width: 150px;"><b>Info/Link</b></div>
   </div>
 </div>
 
 {if !empty($servicelist)}
 {foreach key=count item=nodelist from=$servicelist}
-<div id="nodeitem" style="width: 800px; overflow: hidden;">
+<div id="nodeitem" style="width: 745px; overflow: hidden;">
   <div style="white-space: nowrap;">
-    <div style="float:left; width: 20px;"><b>{$count+1}</b></div>
-    <div style="float:left; width: 95px;"><a href="./service.php?service_id={$nodelist.service_id}">{$net_prefix}.{$nodelist.subnet_ip}.{$nodelist.node_ip}</a></div>
-    <div style="float:left; width: 250px;"><a href="./subnet.php?id={$nodelist.subnet_id}">{$nodelist.title}</a></div>
-    <div style="float:left; width: 110px;"><a href="./user.php?id={$nodelist.user_id}">{$nodelist.nickname}</a></div>
+    <div style="float:left; width: 85px;"><a href="./service.php?service_id={$nodelist.service_id}">{$net_prefix}.{$nodelist.subnet_ip}.{$nodelist.node_ip}</a></div>
+    <div style="float:left; width: 50px;">{$nodelist.crawler}</div>
+
+    <div style="float:left; width: 100px;"><a href="./user.php?id={$nodelist.user_id}">{$nodelist.nickname}</a></div>
     {if $nodelist.status=="online"}
-      <div style="float:left; width: 80px; background-color: green;">{$nodelist.status}</div>
+      <div style="float:left; width: 60px; align: center;"><img src="./templates/img/ffmap/status_up_small.png" alt="online"></div>
     {elseif $nodelist.status=="offline"}
-      <div style="float:left; width: 80px; background-color: red;">{$nodelist.status}</div>
+      <div style="float:left; width: 60px; align: center;"><img src="./templates/img/ffmap/status_down_small.png" alt="offline"></div>
     {elseif $nodelist.status=="ping"}
-      <div style="float:left; width: 80px; background-color: #00c5cc;">{$nodelist.status}</div>
+      <div style="float:left; width: 60px; background-color: #00c5cc;">{$nodelist.status}</div>
     {elseif $nodelist.status=="unbekannt"}
-      <div style="float:left; width: 80px; background-color: #fff3c3;">{$nodelist.status}</div>
+      <div style="float:left; width: 60px; align: center;"><img src="./templates/img/ffmap/status_pending_small.png" alt="offline"></div>
     {/if}
-    <div style="float:left; width: 80px;">{$nodelist.uptime}</div>
-    <div style="float:left; width: 100px;">{$nodelist.crawl_time} Uhr</div>
+    <div style="float:left; width: 150px;">{$nodelist.crawl_time} Uhr</div>
+    <div style="float:left; width: 150px;"><a href="./subnet.php?id={$nodelist.subnet_id}">{$nodelist.title}</a></div>
+    <div style="float:left; width: 150px;">{if is_numeric($nodelist.crawler)}<a href="{if $nodelist.crawler=='80'}http://{elseif $nodelist.crawler=='21'}ftp://{elseif $nodelist.crawler=='8888'}http://{/if}{$net_prefix}.{$nodelist.subnet_ip}.{$nodelist.node_ip}">{$nodelist.services_title|substr:0:20}...</a>{else}&nbsp;{/if}</div>
   </div>
 </div>
 {/foreach}
