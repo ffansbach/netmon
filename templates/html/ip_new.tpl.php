91fd0c5c5d72465d472e7f511439ed65ae9dbbbe
(no author) <(no author)@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2009-11-16 14:00:28 +0000

    Prüfen ob ein gültiger Serice gewählt wurde gefixed

--- templates/html/ip_new.tpl.php
+++ templates/html/ip_new.tpl.php
@@ -5,7 +5,7 @@
 		IP im Subnetz:
 		<select name="subnet_id">
 			{foreach item=subnet from=$existing_subnets}
-				<option value="{$subnet.id}">{$net_prefix}.{$subnet.subnet_ip}.0/24 ({$subnet.title})</option>
+				<option value="{$subnet.id}">{$net_prefix}.{$subnet.host}/{$subnet.netmask} ({$subnet.title})</option>
 			{/foreach}
 		</select> anlegen.
 	</p>
@@ -51,7 +51,7 @@ window.onload = function()
 <p>
 <label id="typLabel" for="typ">Service:</label>
 <select id="typ" name="typ">
-  <option value="--">Bitte wählen:</option>
+  <option value="false">Bitte wählen:</option>
   <option value="node">node</option>
   <option value="vpn">vpn</option>
   <option value="client">client</option>
@@ -61,7 +61,7 @@ window.onload = function()
 <label id="crawlLabel" for="crawl">Crawlart:</label>
 
 <select id="crawl" name="crawler">
-  <option value="--">------</option>
+  <option value="false">erst Service auswählen</option>
 </select>
 
 <span id="portInput" style="visibility:hidden; margin-left: 5px;">
