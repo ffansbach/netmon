0f750f8e321a9a3f99754cdff29313249d0f6ee6
(no author) <(no author)@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2009-12-13 14:06:04 +0000

    JSON API auf Zend-Json-RPC Server umgestellt, bugfixes

--- api_crawl.php
+++ api_crawl.php
@@ -1,9 +1,24 @@
 <?php
 	require_once('./config/runtime.inc.php');
-	require_once './lib/classes/extern/jsonRPCServer.php';
 	require_once('./lib/classes/api/crawl.class.php');
+	require_once 'lib/classes/extern/Zend/Json/Server.php';
 	
-	$myExample = new Crawl();
-	jsonRPCServer::handle($myExample)
-		or print 'no request';
+	$server = new Zend_Json_Server();
+	$server->setClass('Crawl');
+	
+	if ('GET' == $_SERVER['REQUEST_METHOD']) {
+		// Indicate the URL endpoint, and the JSON-RPC version used:
+		$server->setTarget('api_main.php')
+			   ->setEnvelope(Zend_Json_Server_Smd::ENV_JSONRPC_2);
+		
+		// Grab the SMD
+		$smd = $server->getServiceMap();
+		
+		// Return the SMD to the client
+		header('Content-Type: application/json');
+		echo $smd;
+		return;
+	}
+	
+	$server->handle();
 ?>
--- api_main.php
+++ api_main.php
@@ -1,34 +1,24 @@
 <?php
 	require_once('./config/runtime.inc.php');
-	require_once './lib/classes/extern/jsonRPCServer.php';
 	require_once('./lib/classes/api/main.class.php');
-
-	/*
-	$myExample = new Main();
-	jsonRPCServer::handle($myExample)
-		or print 'no request';
-*/
-
-//http://framework.zend.com/manual/de/zend.json.server.html
-require_once 'lib/classes/extern/Zend/Json/Server.php';
-
-$server = new Zend_Json_Server();
-$server->setClass('Main');
-
-if ('GET' == $_SERVER['REQUEST_METHOD']) {
-    // Indicate the URL endpoint, and the JSON-RPC version used:
-    $server->setTarget('api_main.php')
-           ->setEnvelope(Zend_Json_Server_Smd::ENV_JSONRPC_2);
-
-    // Grab the SMD
-    $smd = $server->getServiceMap();
-
-    // Return the SMD to the client
-    header('Content-Type: application/json');
-    echo $smd;
-    return;
-}
-
-$server->handle();
-
+	require_once 'lib/classes/extern/Zend/Json/Server.php';
+	
+	$server = new Zend_Json_Server();
+	$server->setClass('Main');
+	
+	if ('GET' == $_SERVER['REQUEST_METHOD']) {
+		// Indicate the URL endpoint, and the JSON-RPC version used:
+		$server->setTarget('api_main.php')
+			   ->setEnvelope(Zend_Json_Server_Smd::ENV_JSONRPC_2);
+		
+		// Grab the SMD
+		$smd = $server->getServiceMap();
+		
+		// Return the SMD to the client
+		header('Content-Type: application/json');
+		echo $smd;
+		return;
+	}
+	
+	$server->handle();
 ?>
