426e7bf657da0270f48afdcacd4187fe4c698aa3
(no author) <(no author)@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2010-01-28 20:15:52 +0000

    Communityname und Communitywebsite hinzugefügt
    git-svn-id: http://svn.freifunk-ol.de/netmon/Trunk@162 e4279fa6-117a-4c11-8b0b-ff71f4ed7350

--- lib/classes/core/vpn.class.php
+++ lib/classes/core/vpn.class.php
@@ -54,7 +54,7 @@ class Vpn {
 			while ($err = openssl_error_string());
 
 			//Keydaten
-			$dn = array("countryName" => 'Worldwide', "stateOrProvinceName" => 'Worldwide', "localityName" => $GLOBALS['city_name'], "organizationName" => "Netmon Freifunk $GLOBALS[city_name]", "organizationalUnitName" => $organizationalunitname, "commonName" => $commonname, "emailAddress" => $emailaddress);
+			$dn = array("countryName" => 'Worldwide', "stateOrProvinceName" => $GLOBALS['community_name'], "localityName" => 'Netmon', "organizationName" => "$GLOBALS[community_name]", "organizationalUnitName" => $organizationalunitname, "commonName" => $commonname, "emailAddress" => $emailaddress);
 			
 			//Set passphrase to null if no password is given
 			if (empty($privkeypass)) {
