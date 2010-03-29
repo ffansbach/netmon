7b1bcd60f63164f48a8585bb0011ab20e95104a6
(no author) <(no author)@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2010-03-29 21:14:05 +0000

    Imagemaker option für Kernelimage hinzugefügt
    git-svn-id: svn://localhost/netmon/Trunk@183 e4279fa6-117a-4c11-8b0b-ff71f4ed7350

--- templates/html/image_generate.tpl.php
+++ templates/html/image_generate.tpl.php
@@ -11,8 +11,8 @@
 </p>
 
 <h2>Download</h2>
-<a href="./tmp/{$imagepath}/openwrt-atheros-root.squashfs" >openwrt-atheros-root.squashfs</a><br>
-<a href="./tmp/{$imagepath}/openwrt-atheros-vmlinux.lzma" >openwrt-atheros-vmlinux.lzma</a>
+<a href="./tmp/{$imagepath}/openwrt-root.squashfs" >openwrt-root.squashfs</a><br>
+<a href="./tmp/{$imagepath}/openwrt-vmlinux.lzma" >openwrt-vmlinux.lzma</a>
 
 <h3>Optional</h2>
 <a href="./imagemaker.php?section=download_config&imagepath={$imagepath}&ip_id={$smarty.get.ip_id}" >config.zip</a><br>
