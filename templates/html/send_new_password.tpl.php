94520f5fd5f74b0c9d81702a0b2482fbd96e5c00
floh1111 <floh1111@e4279fa6-117a-4c11-8b0b-ff71f4ed7350>
2011-06-24 13:19:10 +0000

    Prepare for new template structure and new oldenburg design

--- templates/html/send_new_password.tpl.php
+++
@@ -1,4 +0,0 @@
-<form action="./send_new_password.php" method="POST">
-  <p>Deine Emailadresse:<br><input name="email" type="text" size="30" maxlength="50"></p>
-  <p><input type="submit" value="Passwort zusenden"></p>
-</form>
--- templates/html/topology.tpl.php
+++
@@ -1,4 +0,0 @@
-<h1>Olsr Netzwerktopology</h1>
-
-<!--<img src="http://icvpn1.freifunk-ol.de/fftopo/topo.png">-->
-<img src="http://olsr.freifunk-ol.de/fftopo/topo.png" width="800px">
