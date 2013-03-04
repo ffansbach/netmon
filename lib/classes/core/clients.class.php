80cba6888d32f8551177e6a776f9b445868845ea
Clemens John <clemens-john@gmx.de>
2013-03-04 20:23:31 +0100

    Move Client class to PDO prepared statements to prevent from sql injections

--- lib/classes/core/clients.class.php
+++ lib/classes/core/clients.class.php
@@ -1,68 +1,54 @@
 <?php
 
+/**
+ * This class is used as a container for static methods that deal with clients (hardware
+ * connected to the routers)
+ *
+ * @package	Netmon
+ */
 class Clients {
-/*	public function getClientsByRouterAndCrawlCycle($router_id, $crawl_cycle_id) {
-		try {
-			$sql = "SELECT  *
-					FROM crawl_clients
-					WHERE crawl_cycle_id='$crawl_cycle_id' AND router_id='$router_id'";
-			$result = DB::getInstance()->query($sql);
-			foreach($result as $row) {
-				$clients[] = $row;
-			}
-		}
-		catch(PDOException $e) {
-			echo $e->getMessage();
-		}
-		return $clients;
-	}
-	public function countClientsByRouterAndCrawlCycle($router_id, $crawl_cycle_id) {
-		try {
-			$sql = "SELECT  COUNT(*) as count
-					FROM crawl_clients
-					WHERE crawl_cycle_id='$crawl_cycle_id' AND router_id='$router_id'";
-			$result = DB::getInstance()->query($sql);
-			$count = $result->fetch(PDO::FETCH_ASSOC);
-		}
-		catch(PDOException $e) {
-			echo $e->getMessage();
-		}
-		return $count['count'];
-	}
-*/
+	/**
+	* Fetches the number of clients that where connected to all routers summed up.
+	* @author  Clemens John <clemens-john@gmx.de>
+	* @param int $crawl_cycle_id id of the crawl cycle you want to get the client number of
+	* @return int number of connected clients
+	*/
 	public function countClientsCrawlCycle($crawl_cycle_id) {
 		$client_count = 0;
 		try {
-			$sql = "SELECT  *
-					FROM crawl_clients_count
-					WHERE crawl_cycle_id='$crawl_cycle_id'";
-			$result = DB::getInstance()->query($sql);
-			foreach($result as $row) {
-				$client_count = $client_count+$row['client_count'];
-			}
-		}
-		catch(PDOException $e) {
+			$stmt = DB::getInstance()->prepare("SELECT * FROM crawl_clients_count WHERE crawl_cycle_id=?");
+			$stmt->execute(array($user_id));
+			$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		} catch(PDOException $e) {
 			echo $e->getMessage();
 		}
+		foreach($rows as $row) {
+			$client_count = $client_count+$row['client_count'];
+		}
+
 		return $client_count;
 	}
 
+	/**
+	* Fetches the number of clients that where connected to a router during a special crawl cycle
+	* @author  Clemens John <clemens-john@gmx.de>
+	* @param int $router_id router id
+	* @param int $crawl_cycle_id id of the crawl cycle you want to get the client number of
+	* @return int number of connected clients
+	*/
 	public function getClientsCountByRouterAndCrawlCycle($router_id, $crawl_cycle_id) {
 		try {
-			$sql = "SELECT  *
-					FROM crawl_clients_count
-					WHERE crawl_cycle_id='$crawl_cycle_id' AND router_id='$router_id'";
-			$result = DB::getInstance()->query($sql);
-			$clients = $result->fetch(PDO::FETCH_ASSOC);
-		}
-		catch(PDOException $e) {
+			$stmt = DB::getInstance()->prepare("SELECT * FROM crawl_clients_count WHERE crawl_cycle_id=? AND router_id=?");
+			$stmt->execute(array($crawl_cycle_id, $router_id));
+			$rows = $stmt->fetch(PDO::FETCH_ASSOC);
+		} catch(PDOException $e) {
 			echo $e->getMessage();
 		}
-		if(!empty($clients)) {
-			return $clients['client_count'];
-		} else {
+		
+		if(!empty($clients))
+			return $rows['client_count'];
+		else
 			return 0;
-		}
 	}
 }
 
