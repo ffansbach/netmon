6da958e808afe51af32a42f8dca0d5b30193a39c
Clemens John <clemens-john@gmx.de>
2013-03-09 00:36:32 +0100

    Move History class to PDO prepared statements to prevent from sql injections

--- lib/classes/core/history.class.php
+++ lib/classes/core/history.class.php
@@ -81,12 +81,12 @@ class History {
 	}
 
 	public function getRouterHistoryByRouterId($router_id, $countlimit, $hourlimit) {
-		if($countlimit)
+		if($countlimit AND is_numeric($countlimit) AND is_numeric($router_id))
 			$range = "
 						WHERE object='router' AND object_id=$router_id
 						ORDER BY history.create_date desc
 					  LIMIT 0, $countlimit";
-		elseif ($hourlimit)
+		elseif ($hourlimit AND is_numeric($hourlimit))
 			$range = "WHERE history.create_date>=NOW() - INTERVAL $hourlimit HOUR AND object='service'
 					  ORDER BY history.create_date desc";
 		try {
@@ -108,12 +108,12 @@ class History {
 	}
 
 	public function getRouterHistoryByRouterIdExceptActualCrawlCycle($router_id, $actual_crawl_cycle_id, $countlimit, $hourlimit) {
-		if($countlimit)
+		if($countlimit AND is_numeric($countlimit) AND is_numeric($router_id))
 			$range = "
 						WHERE object='router' AND object_id=$router_id AND crawl_cycle_id<'$actual_crawl_cycle_id'
 						ORDER BY history.create_date desc
 					  LIMIT 0, $countlimit";
-		elseif ($hourlimit)
+		elseif ($hourlimit AND is_numeric($hourlimit) AND is_numeric($actual_crawl_cycle_id))
 			$range = "WHERE history.create_date>=NOW() - INTERVAL $hourlimit HOUR AND object='service' AND crawl_cycle_id<'$actual_crawl_cycle_id'
 					  ORDER BY history.create_date desc";
 		try {
@@ -137,38 +137,37 @@ class History {
 	public function getUserHistory($user_id, $countlimit) {
 		$actual_crawl_cycle = Crawling::getActualCrawlCycle();
 		$history_cp = array();
-
+		
 		try {
-			$sql = "SELECT  *
-					FROM routers
-					WHERE user_id='$user_id'";
-			$result = DB::getInstance()->query($sql);
-			foreach($result as $row) {
-				$routers[] = $row;
-			}
-		}
-		catch(PDOException $e) {
+			$stmt = DB::getInstance()->prepare("SELECT  * FROM routers WHERE user_id=?");
+			$stmt->execute(array($user_id));
+			$routers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		} catch(PDOException $e) {
 			echo $e->getMessage();
-		}
+			echo $e->getTraceAsString();
+		}		
 		if(!empty($routers)) {
 		foreach($routers as $router) {
 			try {
-				$sql = "SELECT id, object, object_id, create_date, data
+				$stmt = DB::getInstance()->prepare("SELECT id, object, object_id, create_date, data
 					FROM history
-					WHERE object='router' AND object_id=$router[id] AND crawl_cycle_id<'$actual_crawl_cycle[id]'
+					WHERE object='router' AND object_id=:router_id AND crawl_cycle_id<:crawl_cycle_id
 					ORDER BY history.create_date desc
-					LIMIT 0, $countlimit";
-					
-				$result = DB::getInstance()->query($sql);
-				foreach($result as $key=>$row) {
-					$history[$key] = $row;
-					$history[$key]['data'] = unserialize($history[$key]['data']);
-					$history[$key]['additional_data'] = Router::getRouterInfo($row['object_id']);
-					$user_history[] = $history[$key];
-				}
-			}
-			catch(PDOException $e) {
+					LIMIT 0, :limit");
+					$stmt->bindValue(':limit', (int)$countlimit, PDO::PARAM_INT);
+					$stmt->bindValue(':router_id', (int)$router['id'], PDO::PARAM_INT);
+					$stmt->bindValue(':crawl_cycle_id', (int)$actual_crawl_cycle['id'], PDO::PARAM_INT);
+					$stmt->execute();
+					$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			} catch(PDOException $e) {
 				echo $e->getMessage();
+				echo $e->getTraceAsString();
+			}
+			foreach($result as $key=>$row) {
+				$history[$key] = $row;
+				$history[$key]['data'] = unserialize($history[$key]['data']);
+				$history[$key]['additional_data'] = Router::getRouterInfo($row['object_id']);
+				$user_history[] = $history[$key];
 			}
 		}
 
@@ -192,32 +191,32 @@ class History {
 		$history = array();
 		$actual_crawl_cycle = Crawling::getActualCrawlCycle();
 		if(isset($actual_crawl_cycle['id']) AND (isset($countlimit) OR isset($hourlimit))) {
-			if($countlimit)
+			if($countlimit AND is_numeric($countlimit))
 				$range = "
 						WHERE history.crawl_cycle_id!=$actual_crawl_cycle[id]
 						ORDER BY history.create_date desc
 						LIMIT 0, $countlimit";
-			elseif ($hourlimit)
+			elseif ($hourlimit AND is_numeric($hourlimit))
 				$range = "WHERE history.create_date>=NOW() - INTERVAL $hourlimit HOUR AND history.crawl_cycle_id!=$actual_crawl_cycle[id]
 						ORDER BY history.create_date desc";
+						
 			try {
-				$sql = "SELECT id, object, object_id, create_date, data
-					FROM history
-					$range";
-				$result = DB::getInstance()->query($sql);
-				foreach($result as $key=>$row) {
-					$history[$key] = $row;
-					$history[$key]['data'] = unserialize($history[$key]['data']);
-					if($history[$key]['object'] == "router") {
-						$history[$key]['additional_data'] = Router::getRouterInfo($row['object_id']);
-					}
-				}
-			}
-			catch(PDOException $e) {
+				$stmt = DB::getInstance()->prepare("SELECT id, object, object_id, create_date, data FROM history $range");
+				$stmt->execute();
+				$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			} catch(PDOException $e) {
 				echo $e->getMessage();
+				echo $e->getTraceAsString();
+			}
+			foreach($rows as $key=>$row) {
+				$history[$key] = $row;
+				$history[$key]['data'] = unserialize($history[$key]['data']);
+				if($history[$key]['object'] == "router") {
+					$history[$key]['additional_data'] = Router::getRouterInfo($row['object_id']);
+				}
 			}
 			return $history;
 		}
-	}	
+	}
 }
 ?>
