7bf2495f72fcc7742336d90ac5874e188752490a
Clemens John <clemens-john@gmx.de>
2013-09-19 02:10:12 +0000

    Fix units and scale of traffic on routerpage, show systemmessages on new router page

--- routers_trying_to_assign.php
+++ routers_trying_to_assign.php
@@ -1,11 +1,13 @@
 <?php
-  require_once('runtime.php');
-  require_once('lib/core/routersnotassigned.class.php');
-  
-  $routerlist = RoutersNotAssigned::getRouters();
-  $smarty->assign('routerlist', $routerlist);
-
-  $smarty->display("header.tpl.html");
-  $smarty->display("routers_trying_to_assign.tpl.html");
-  $smarty->display("footer.tpl.html");
+	require_once('runtime.php');
+	require_once('lib/core/routersnotassigned.class.php');
+	
+	$smarty->assign('message', Message::getMessage());
+	
+	$routerlist = RoutersNotAssigned::getRouters();
+	$smarty->assign('routerlist', $routerlist);
+	
+	$smarty->display("header.tpl.html");
+	$smarty->display("routers_trying_to_assign.tpl.html");
+	$smarty->display("footer.tpl.html");
 ?>
