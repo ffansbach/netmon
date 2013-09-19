4d378b91b60f2a66bf3e94ff33a8c1166925bc34
Clemens John <clemens-john@gmx.de>
2013-09-19 01:46:19 +0000

    Increase performance, performance now allows live update

--- scripts/ffmap/nodes.php
+++ scripts/ffmap/nodes.php
@@ -25,49 +25,24 @@
 	$routers = $xpath->query('/netmon_response/routerlist/router');
 	foreach($routers as $router) {
 		//parse node values of the nodes inside the router node
-		$node_flags = array(
-					'client' => false,
-					'gateway' => false,
-					'online' => (($xpath->evaluate('string(./statusdata/status/text())', $router)=='online') ? true : false));
-		$node_geo = array((float)$xpath->evaluate('string(./latitude/text())', $router),
-						  (float)$xpath->evaluate('string(./longitude/text())', $router)
-						 );
-		
-		$node_id = $xpath->evaluate('string(./router_id/text())', $router);
-		
-		//get mac addresses
-		$doc_networkinterfaces = new DOMDocument();
-		$doc_networkinterfaces->load("http://netmon.freifunk-ol.de/api/rest/router/".$node_id."/networkinterfacelist?offset=0&limit=-1");
-		$xpath_interfaces = new DOMXPath($doc_networkinterfaces);
-		
-		$mac_addresses = $xpath_interfaces->query('/netmon_response/networkinterfacelist/networkinterface/statusdata/mac_addr');
-		$node_macs = "";
-		$prefix = "";
-		foreach($mac_addresses as $mac_addr) {
-			$mac_addr = $xpath_interfaces->evaluate('string(./text())', $mac_addr);
-			if(!empty($mac_addr)) {
-				$node_macs .= $prefix.$mac_addr;
-				$prefix = ", ";
-			}
-		}
-		
 		$node = array(
-					'flags' => $node_flags,
-					'geo' => $node_geo,
-					'macs' => $node_macs,
+					'flags' => array('client' => false,
+									 'gateway' => false,
+									 'online' => (($xpath->evaluate('string(./statusdata/status/text())', $router)=='online') ? true : false)),
+					'geo' => array((float)$xpath->evaluate('string(./latitude/text())', $router),
+								   (float)$xpath->evaluate('string(./longitude/text())', $router)),
+					'macs' => "",
 					'name' => $xpath->evaluate('string(./hostname/text())', $router),
-					'id' => $node_id);
+					'id' => $xpath->evaluate('string(./router_id/text())', $router));
 		$node_index = array_push($nodes, $node)-1;
 		
 		//get clients
 		$client_count = (int)$xpath->evaluate('number(./statusdata/client_count/text())', $router);
 		for($j=0; $j<$client_count; $j++) {
-			$client_flags = array(
-							'client' => true,
-							'gateway' => false,
-							'online' => true);
 			$client = array(
-						'flags' => $client_flags,
+						'flags' => array('client' => true,
+										 'gateway' => false,
+										 'online' => true),
 						'geo' => null,
 						'macs' => "",
 						'name' => "",
@@ -82,42 +57,58 @@
 							'type' => 'client');
 			$links[] = $client_link;
 		}
-		
-		//get originators
-		$doc_originators = new DOMDocument();
-		$doc_originators->load("http://netmon.freifunk-ol.de/api/rest/router/".$node_id."/originator_status_list?offset=0&limit=-1");
-		$xpath_originators = new DOMXPath($doc_originators);
-		$originator_status_list = $xpath_originators->query('/netmon_response/originator_status_list/originator_status');
-		foreach($originator_status_list as $originator) {
-			$outgoing_interface = trim($xpath_originators->evaluate('string(./outgoing_interface/text())', $originator));
-			$link_quality = trim($xpath_originators->evaluate('string(./link_quality/text())', $originator));
-			$nexthop = trim($xpath_originators->evaluate('string(./nexthop/text())', $originator));
-			$originator = trim($xpath_originators->evaluate('string(./originator/text())', $originator));
-			
-			//put all direct neighbours into tmp originator array
-			if($originator==$nexthop) {
-				$node_originators[] = array(
-										'node_id' => $node['id'],
-										'node_index' => $node_index,
-										'originator' => $originator,
-										'quality' => $link_quality,
-										'outgoing_interface' => $outgoing_interface);
+	}
+	
+	//get mac addresses
+	$doc_networkinterfaces = new DOMDocument();
+	$doc_networkinterfaces->load("http://netmon.freifunk-ol.de/api/rest/networkinterfacelist?offset=0&limit=-1");
+	$xpath_interfaces = new DOMXPath($doc_networkinterfaces);
+	$interfaces = $xpath_interfaces->query('/netmon_response/networkinterfacelist/networkinterface');
+	foreach($interfaces as $interface) {
+		$node_id = $xpath_interfaces->evaluate('string(./router_id/text())', $interface);
+		foreach($nodes as $key=>$node) {
+			if($node_id == $node['id']) {
+				if($nodes[$key]['macs']!="")
+					$nodes[$key]['macs'] .= ", ";
+				$nodes[$key]['macs'] .= $xpath_interfaces->evaluate('string(./statusdata/mac_addr/text())', $interface);
+				break;
 			}
 		}
 	}
 	
-	foreach($node_originators as $originator) {
-		foreach($nodes as $key=>$node) {
-			if(stripos($node['macs'], $originator['originator'])!==false) {
-				$type = (stripos($originator['outgoing_interface'], 'VPN')!==false) ? "vpn" : "wlan";
+	//get originators
+	$doc_originators = new DOMDocument();
+	$doc_originators->load("http://netmon.freifunk-ol.de/api/rest/originator_status_list?offset=0&limit=-1");
+	$xpath_originators = new DOMXPath($doc_originators);
+	$originator_status_list = $xpath_originators->query('/netmon_response/originator_status_list/originator_status');
+	foreach($originator_status_list as $originator_status) {
+		$node_id = $outgoing_interface = trim($xpath_originators->evaluate('string(./router_id/text())', $originator_status));
+		$outgoing_interface = trim($xpath_originators->evaluate('string(./outgoing_interface/text())', $originator_status));
+		$link_quality = trim($xpath_originators->evaluate('string(./link_quality/text())', $originator_status));
+		$nexthop = trim($xpath_originators->evaluate('string(./nexthop/text())', $originator_status));
+		$originator = trim($xpath_originators->evaluate('string(./originator/text())', $originator_status));
+		$type = (stripos($outgoing_interface, 'VPN')!==false) ? "vpn" : "wlan";
+
+		//put all direct neighbours into tmp originator array
+		if($originator==$nexthop) {
+			$source = 0;
+			$target = 0;
+			foreach($nodes as $key=>$node) {
+				if(stripos($node['macs'], $originator)!==false)
+					$target = $key;
+				elseif($node['id']==$node_id)
+					$source = $key;
 				
-				//TODO: mege quality from the second link and build one link only
-				$links[] = array(
-							'id' => $originator['node_id']."-".$node['id'],
-							'source' => $originator['node_index'],
-							'quality' => (($originator['quality']*(-1)+255)/10).", ".(($originator['quality']*(-1)+255)/10),
-							'target' => $key,
-							'type' => $type);
+				if($source!=0 AND $target!=0) {
+					//TODO: mege quality from the second link and build one link only
+					$links[] = array(
+								'id' => $nodes[$source]['id']."-".$nodes[$target]['id'],
+								'source' => $source,
+								'quality' => (($link_quality*(-1)+255)/10).", ".(($link_quality*(-1)+255)/10),
+								'target' => $target,
+								'type' => $type);
+					break;
+				}
 			}
 		}
 	}
@@ -126,8 +117,7 @@
 	$mydocument = array(
 					'nodes' => $nodes,
 					'meta' => $meta,
-					'links' => $links,
-					'originators' => $node_originators);
+					'links' => $links);
 	
 //	print_r($mydocument);
 	//encode the array to json and output
