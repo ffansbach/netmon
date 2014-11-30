d5873e564ee9575cb3c639d167271943057ac645
Clemens John <clemens-john@gmx.de>
2014-11-30 17:41:10 +0100

    Add embed mode to map. Use like http://netmon.nordwest.freifunk.net/map.php?embed=true&longitude=8.0545&latitude=52.2736&zoom=12 for Osnabrueck i.e.
    Signed-off-by: Clemens John <clemens-john@gmx.de>

--- map.php
+++ map.php
@@ -1,12 +1,30 @@
 <?php
-  require_once('runtime.php');
-
-  $smarty->assign('community_location_longitude', Config::getConfigValueByName('community_location_longitude'));
-  $smarty->assign('community_location_latitude', Config::getConfigValueByName('community_location_latitude'));
-  $smarty->assign('community_location_zoom', Config::getConfigValueByName('community_location_zoom'));
-  $smarty->assign('google_maps_api_key', $GLOBALS['google_maps_api_key']);
-  
-  $smarty->display("header.tpl.html");
-  $smarty->display("map.tpl.html");
-  $smarty->display("footer.tpl.html");
+	require_once('runtime.php');
+	
+	if(isset($_GET['embed']) AND $_GET['embed']) {
+		if(!isset($_GET['longitude'])
+		   OR !isset($_GET['latitude'])
+		   OR !isset($_GET['zoom'])) {
+			$smarty->assign('community_location_longitude', Config::getConfigValueByName('community_location_longitude'));
+			$smarty->assign('community_location_latitude', Config::getConfigValueByName('community_location_latitude'));
+			$smarty->assign('community_location_zoom', Config::getConfigValueByName('community_location_zoom'));
+		} else {
+			$smarty->assign('community_location_longitude', $_GET['longitude']);
+			$smarty->assign('community_location_latitude', $_GET['latitude']);
+			$smarty->assign('community_location_zoom', $_GET['zoom']);
+		}
+		
+		$smarty->assign('google_maps_api_key', $GLOBALS['google_maps_api_key']);
+		
+		$smarty->display("map_embed.tpl.html");
+	} else {
+		$smarty->assign('community_location_longitude', Config::getConfigValueByName('community_location_longitude'));
+		$smarty->assign('community_location_latitude', Config::getConfigValueByName('community_location_latitude'));
+		$smarty->assign('community_location_zoom', Config::getConfigValueByName('community_location_zoom'));
+		$smarty->assign('google_maps_api_key', $GLOBALS['google_maps_api_key']);
+		
+		$smarty->display("header.tpl.html");
+		$smarty->display("map.tpl.html");
+		$smarty->display("footer.tpl.html");
+	}
 ?>
