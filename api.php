38e4900066625e3fe765a8ccc75aba5698720489
Stefan Siegl <stesie@brokenpipe.de>
2015-03-22 00:24:58 +0100

    Check class & section GET/POST arg for validity
    Before this fix these allowed for execution of arbitrary code.
    
    The situation is still bad after applying this patch, since
    it's still allowed to execute any PHP method without further
    arguments; but it at least keeps attackers from calling
    shell_exec et al

--- api.php
+++ api.php
@@ -4,9 +4,11 @@
 
 class Api {
   function __construct() {
-	if (isset($_GET['class']) AND isset($_GET['section']))
+	if (isset($_GET['class']) AND class_exists($_GET['class'])
+            AND isset($_GET['section']) AND method_exists($_GET['class'], $_GET['section']))
 		eval($_GET['class']."::".$_GET['section']."();");
-	elseif(isset($_POST['class']) AND isset($_POST['section']))
+	elseif(isset($_POST['class']) AND class_exists($_POST['class'])
+            AND isset($_POST['section']) AND method_exists($_POST['class'], $_POST['section']))
 		eval($_POST['class']."::".$_POST['section']."();");
 	else
 		echo "data incomplete";
