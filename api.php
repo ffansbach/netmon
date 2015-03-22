1d3bb40ea9fc3d9698b7a1c38b75b988553bbc3b
Clemens John <clemens.john@floh1111.de>
2015-03-22 10:07:35 +0100

    Prevent execution from methods of classes required by runtime.php
    Signed-off-by: Clemens John <clemens.john@floh1111.de>

--- api.php
+++ api.php
@@ -5,7 +5,7 @@
 class Api {
 	function __construct() {
 		if (isset($_REQUEST['class']) AND isset($_REQUEST['section'])) {
-			if(method_exists($_REQUEST['class'], $_REQUEST['section'])) {
+			if($_REQUEST['class'] == 'ApiMap' && method_exists($_REQUEST['class'], $_REQUEST['section'])) {
 				eval($_REQUEST['class']."::".$_REQUEST['section']."();");
 			} else {
 				echo "Class or section does not exist.";
