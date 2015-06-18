74478c5640adf163edf33bbf744c42975b672ada
Stefan Siegl <stesie@brokenpipe.de>
2015-06-19 01:34:14 +0200

    fix inet_to_bits use of unpack
    PHP 5.5 changed behaviour of unpack wrt. to trailing null bytes.

--- lib/core/Ip.class.php
+++ lib/core/Ip.class.php
@@ -318,8 +318,8 @@
 		// converts inet_pton output to string with bits
 		//http://stackoverflow.com/questions/7951061/matching-ipv6-address-to-a-cidr-subnet
 		public static function inet_to_bits($inet, $ipv) {
-			if($ipv==4) $unpacked = unpack('A4', $inet);
-			elseif($ipv==6) $unpacked = unpack('A16', $inet);
+			if($ipv==4) $unpacked = unpack('a4', $inet);
+			elseif($ipv==6) $unpacked = unpack('a16', $inet);
 			
 			$unpacked = str_split($unpacked[1]);
 			$binaryip = '';
